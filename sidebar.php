5c0b344ef30defa4b6e209826b08caeabbefd581
Blair Learn <thatblairguy@users.noreply.github.com>
2021-03-14 21:43:47 -0400

    Remove WordPress  administrative links from sidebar.

--- sidebar.php
+++ sidebar.php
@@ -56,19 +56,6 @@
 
 
 
-			<?php /* If this is the frontpage */ if ( is_home() || is_page() ) { ?>
-				<li><h2>Administration</h2>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php /* <li><a href="http://validator.w3.org/check/referer" title="This page validates as XHTML 1.0 Transitional">Valid <abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a></li> */ ?>
-					<li><a href="http://gmpg.org/xfn/"><abbr title="XHTML Friends Network">XFN</abbr></a></li>
-					<li><a href="http://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress</a></li>
-					<?php wp_meta(); ?>
-				</ul>
-				</li>
-			<?php } ?>
-
 		</ul>
 	</div>
 
