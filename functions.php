577ec8eec3d338095fb2d463fee08ec188bec66f
Blair Learn <thatblairguy@users.noreply.github.com>
2021-03-14 21:45:46 -0400

    Remove links to dynamically generated content
    Removed items include emoji, shortlinks, generator meta  tag, discovery
    links, and various other rel= links/attributes.

--- functions.php
+++ functions.php
@@ -394,4 +394,58 @@ function kubrick_theme_page() {
 		</div>
 	</div>
 </div>
-<?php } ?>
+<?php }
+
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+
+/**
+ * Disable the emoji
+ */
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );	
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	
+	// Remove from TinyMCE
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+add_action( 'init', 'disable_emojis' );
+
+/**
+ * Filter out the tinymce emoji plugin.
+ */
+function disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+}
+
+remove_action('wp_head', 'wp_generator');
+
+remove_action( 'wp_head', 'rest_output_link_wp_head' );
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+remove_action( 'template_redirect', 'rest_output_link_header', 11 );
+
+# remove_action( 'wp_head', 'feed_links', 2 );
+add_filter( 'feed_links_show_comments_feed', '__return_false' );
+
+# From https://stackoverflow.com/a/34750194/282194
+remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
+remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+remove_action( 'wp_head', 'index_rel_link' ); // index link
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
+
+
+
+
+?>
